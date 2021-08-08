32a47b045aaa67e13ef47976158f7176d62d0f1e
SosaMati <matiasezequielsosa@gmail.com>
2021-08-08 20:00:34 -0300

    Updated

---
+++ footer.js
@@ -0,0 +1,21 @@
+var texto_footer_html = `    
+                            <div> 
+                             
+                              
+                                  <h5 >872328738<i class="fas fa-phone-square-alt"></i><br>
+                                  <a class="datos" href= "mailto:ksjksdksdk@gmail.com"> ejemplo@mail.com <i class="far fa-envelope"></i></a></h5>
+
+                              
+                            </div>
+
+                            <br>
+
+
+                            <div>
+                              <a class="iconfooter" href="http://www.twitter.com"><i class="fab fa-twitter"></i></a>
+                              <a class="iconfooter" href="http://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                              <a class="iconfooter" href="http://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                            </div>   `
+
+document.getElementById("pie").innerHTML =texto_footer_html;                            
+
---
+++ header.js
@@ -0,0 +1,22 @@
+var texto_header_html = `<h1>Vivir viajando  <i class="fas fa-mountain"></i></h1>
+                         <div id="menu">
+                           <nav class="navbar navbar-expand-lg navbar-dark">
+                            <div class="container-fluid">
+                               <a class="navbar-brand" href="#">Menú</a>
+                              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                               <span class="navbar-toggler-icon"></span>
+                              </button>
+                              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                               <div class="navbar-nav">
+                                  <a class="nav-link" aria-current="page" href="/Index.html">Home</a>
+                                  <a class="nav-link" href="/recorrido.html">Recorrido</a>
+                                  <a class="nav-link" href="/galeriadefotos.html">Galeria de fotos</a>
+                                  <a class="nav-link" href="/ubicaciones.html">Ubicaciones</a>
+                                  <a class="nav-link" href="/contacto.html">Contacto</a>
+                               </div>
+                             </div>
+                           </div>
+                         </nav>
+                        </div> `
+
+document.getElementById("encabezado").innerHTML = texto_header_html;                    
